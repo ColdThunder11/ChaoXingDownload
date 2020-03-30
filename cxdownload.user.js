15a8dd90f15f51e05daabdae8f9c93e61d1b9649
ColdThunder11 <34985592+ColdThunder11@users.noreply.github.com>
2020-03-30 15:40:40 +0800

    add download all page

--- cxdownload.user.js
+++ cxdownload.user.js
@@ -14,6 +14,8 @@
 (function() {
     'use strict';
     setInterval(()=>{
+        var haveResource=false;
+        var downloadLinks=new Array();
         var iframes=document.getElementsByTagName("iframe");
         for(var i=0;i<iframes.length;i++){
             var frames=iframes[i].contentWindow.document.getElementsByTagName("iframe");
@@ -27,26 +29,52 @@
                 if(data!=null){
                     var jsondata=JSON.parse(data);
                     if(jsondata.type==".ppt"||jsondata.type==".pptx"||jsondata.type==".mp4"||jsondata.type==".pdf"||jsondata.type==".flv"||jsondata.type==".doc"||jsondata.type==".docx"||jsondata.type==".avi"){
+                        if(!haveResource) haveResource=true;
+                        downloadLinks.push("https://d0.ananas.chaoxing.com/download/"+jsondata.objectid)
                         var downloadTag = document.createElement("A");
                         downloadTag.setAttribute("href","https://d0.ananas.chaoxing.com/download/"+jsondata.objectid);
                         downloadTag.setAttribute("class","ct11_dl");
+                        downloadTag.setAttribute("style","font-size: 14px;color: #666666;");
                         downloadTag.innerHTML="点此下载 "+jsondata.name;
                         fdiv.appendChild(downloadTag);
-                        console.log(frame);
                         continue;
                     }
                 }
                 if(frame.getAttribute("name")==null) continue;
                 if(frame.getAttribute("name").substr(frame.getAttribute("name").length-4,4)==".mp3"){
+                    if(!haveResource) haveResource=true;
+                    downloadLinks.push("https://d0.ananas.chaoxing.com/download/"+frame.getAttribute("objectid"))
                     var adownloadTag = document.createElement("A");
                     adownloadTag.setAttribute("href","https://d0.ananas.chaoxing.com/download/"+frame.getAttribute("objectid"));
                     adownloadTag.setAttribute("class","ct11_dl");
+                    adownloadTag.setAttribute("style","font-size: 14px;color: #666666;");
                     adownloadTag.innerHTML="点此下载 "+frame.getAttribute("name");
                     fdiv.appendChild(adownloadTag);
                     continue;
                 }
 
             }
+        }
+        if(haveResource){
+            if(iframes[0].parentNode.getElementsByClassName("ct11_dl")[0]!=null) iframes[0].parentNode.getElementsByClassName("ct11_dl")[0].remove
+            var allDownloadTag = document.createElement("A");
+            allDownloadTag.setAttribute("class","ct11_dl");
+            allDownloadTag.setAttribute("style","font-size: 14px;color: #666666;");
+            allDownloadTag.setAttribute("href","javascript:void(0)");
+            allDownloadTag.innerHTML="点此下载本节内的全部资源";
+            allDownloadTag.onclick = function name(params) {
+                for(var i=0;i<downloadLinks.length;i++){
+                    const iiframe = document.createElement("iframe");
+                    iiframe.style.display = "none";
+                    iiframe.style.height = 0;
+                    iiframe.src = downloadLinks[i];
+                    document.body.appendChild(iiframe);
+                    setTimeout(()=>{
+                        iiframe.remove();
+                    }, 10000);
+                }
+            }
+            iframes[0].parentNode.insertBefore(allDownloadTag,iframes[0])
         }
     },3000);
 })();
