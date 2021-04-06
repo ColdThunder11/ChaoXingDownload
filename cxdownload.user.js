a27dd2e73282f5157b69ba972c899b9fb859dd8e
ColdThunder11 <34985592+ColdThunder11@users.noreply.github.com>
2021-04-06 16:54:11 +0800

    replace a tag wuth div

--- cxdownload.user.js
+++ cxdownload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         超星学习通课程资源直链下载
 // @namespace    https://github.com/ColdThunder11/ChaoXingDownload
-// @version      0.28
+// @version      0.29
 // @description  超星学习通课程资源直链下载，支持ppt(x),doc(x),pdf,mp4,flv,mp3,avi资源的下载，支持整节课资源批量下载。
 // @author       ColdThunder11
 // @match        *://*.chaoxing.com/mycourse/studentstudy?chapterId=*&courseId=*&clazzid=*&enc=*
@@ -18,7 +18,6 @@
 // @match        *://*.edu.cn/coursedata?courseId=*&dataName=*&dataId=*&type=*&parent=*&flag=*&classId=*&enc*&ut=*&cpi=*&openc=*
 // @grant        none
 // @supportURL   https://github.com/ColdThunder11/ChaoXingDownload/issues
-// @updateURL    https://github.com/ColdThunder11/ChaoXingDownload/raw/master/cxdownload.user.js
 // ==/UserScript==
 
 
@@ -67,11 +66,15 @@
                                 downloadLinks=new Array();
                             }
                             downloadLinks.push("https://cs-ans.chaoxing.com/download/"+jsondata.objectid)
-                            var downloadTag = document.createElement("P");
+                            var downloadTag = document.createElement("div");
+                            downloadTag.setAttribute("href","javascript:void(0)");
                             //downloadTag.setAttribute("href","https://cs-ans.chaoxing.com/download/"+jsondata.objectid);
                             downloadTag.setAttribute("class","ct11_dl");
-                            downloadTag.setAttribute("style","font-size: 14px;color: #666666;");
-                            downloadTag.innerHTML=jsondata.name+" 的下载链接：<br> https://cs-ans.chaoxing.com/download/"+jsondata.objectid;
+                            downloadTag.setAttribute("style","font-size: 14px;color: #666666;cursor:pointer;");
+                            downloadTag.innerHTML="点此下载 "+jsondata.name;
+                            downloadTag.onclick = function name(params) {
+                                window.location = "https://cs-ans.chaoxing.com/download/"+jsondata.objectid;
+                            }
                             fdiv.appendChild(downloadTag);
                             continue;
                         }
@@ -83,22 +86,24 @@
                             downloadLinks=new Array();
                         }
                         downloadLinks.push("https://cs-ans.chaoxing.com/download/"+frame.getAttribute("objectid"))
-                        var adownloadTag = document.createElement("P");
+                        var adownloadTag = document.createElement("div");
+                        adownloadTag.setAttribute("href","javascript:void(0)");
                         //adownloadTag.setAttribute("href","https://cs-ans.chaoxing.com/download/"+frame.getAttribute("objectid"));
                         adownloadTag.setAttribute("class","ct11_dl");
-                        adownloadTag.setAttribute("style","font-size: 14px;color: #666666;");
-                        adownloadTag.innerHTML=jsondata.name+" 的下载链接：<br> https://cs-ans.chaoxing.com/download/"+frame.getAttribute("objectid");
-                        fdiv.appendChild(adownloadTag);
+                        adownloadTag.setAttribute("style","font-size: 14px;color: #666666;cursor:pointer;");
+                        adownloadTag.innerHTML="点此下载 "+frame.getAttribute("name");
+                        adownloadTag.onclick = function name(params) {
+                            window.location = "https://cs-ans.chaoxing.com/download/"+frame.getAttribute("objectid");
+                        }
                         continue;
                     }
-
                 }
             }
             if(haveResource){
                 if(iframes[0].parentNode.getElementsByClassName("ct11_dl")[0]!=null) iframes[0].parentNode.getElementsByClassName("ct11_dl")[0].remove()
-                var allDownloadTag = document.createElement("A");
+                var allDownloadTag = document.createElement("div");
                 allDownloadTag.setAttribute("class","ct11_dl");
-                allDownloadTag.setAttribute("style","font-size: 14px;color: #666666;");
+                allDownloadTag.setAttribute("style","font-size: 14px;color: #666666;cursor:pointer;");
                 allDownloadTag.setAttribute("href","javascript:void(0)");
                 allDownloadTag.innerHTML="点此下载本节内的全部资源";
                 allDownloadTag.onclick = function name(params) {
